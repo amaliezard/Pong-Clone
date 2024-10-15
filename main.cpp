e267770b837e1f0527432400fdd41e828672ff09
amaliezard <zardamalie@gmail.com>
2024-10-14 23:16:16 -0400

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+/**
+* Author: Amalie Zard
+* Assignment: Pong Clone 
+* Date due: 2024-14-10, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 #include <SDL.h>
